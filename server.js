8ff488a60cde262b8d8cacda67fce626dfdaa7e9
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2021-08-16 17:29:23 +0100

    Add 3rd intro line. Fix question order config

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ var serviceUserTypeB = process.env.SERVICE_USER_TYPE_B
 
 // JHS 131019 make questionOrder global so can access it in routes
 
-questionOrder = process.env.QUESTION_ORDER || config.questionOrder
+questionOrder = process.env.QUESTION_ORDER ? Array.from(process.env.QUESTION_ORDER) : config.questionOrder
 
 useHttps = useHttps.toLowerCase()
 
