ce1363887451e6b745cc6c9eedf3932d12d1efb9
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2022-11-08 16:24:20 +0000

    Remove findAvailablePort function

--- listen-on-port.js
+++ listen-on-port.js
@@ -5,28 +5,26 @@ const browserSync = require('browser-sync')
 // Local dependencies
 const server = require('./server.js')
 const config = require('./app/config.js')
-const utils = require('./lib/utils.js')
 
 // Set up configuration variables
-var useBrowserSync = config.useBrowserSync.toLowerCase()
-var env = (process.env.NODE_ENV || 'development').toLowerCase()
+const useBrowserSync = config.useBrowserSync.toLowerCase()
+const env = (process.env.NODE_ENV || 'development').toLowerCase()
+const port = process.env.PORT || 3001
 
-utils.findAvailablePort(server, function (port) {
-  console.log('Listening on port ' + port + '   url: http://localhost:' + port)
-  if (env === 'production' || useBrowserSync === 'false') {
-    server.listen(port)
-  } else {
-    server.listen(port - 50, function () {
-      browserSync({
-        proxy: 'localhost:' + (port - 50),
-        port: port,
-        ui: false,
-        files: ['public/**/*.*', 'app/views/**/*.*'],
-        ghostmode: false,
-        open: false,
-        notify: false,
-        logLevel: 'error'
-      })
+console.log('Listening on port ' + port + '   url: http://localhost:' + port)
+if (env === 'production' || useBrowserSync === 'false') {
+  server.listen(port)
+} else {
+  server.listen(port - 50, function () {
+    browserSync({
+      proxy: 'localhost:' + (port - 50),
+      port: port,
+      ui: false,
+      files: ['public/**/*.*', 'app/views/**/*.*'],
+      ghostmode: false,
+      open: false,
+      notify: false,
+      logLevel: 'error'
     })
-  }
-})
+  })
+}
